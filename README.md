1486933de121d94eac513861112e79b9d1427e7d
The Count <70529195+count-null@users.noreply.github.com>
2021-01-19 00:09:25 -0500

    ready to trade

--- README.md
+++ README.md
@@ -39,22 +39,18 @@ Edit `config.json` to your liking.
 
 ## Config Options
 
-### alts 
-
 This is a list of rules for each alt eligible to trade. Here are some examples:
 ```
-{
-  "name": "ETH",
-  "minBalance": 0.5,
+"ETH": {
+  "minBalance": 0,
   "quote": "ETH",
   "buy": "XBT"
 },
 ```
-Sell ETH for XBT. Keep a minimum of 0.5 ETH
+Sell ETH for XBT. Sell all of it. 
 
 ```
-{
-  "name": "STORJ",
+"STORJ": {
   "minBalance": 50,
   "quote": "USD",
   "buy": "XBT"
@@ -63,14 +59,11 @@ Sell ETH for XBT. Keep a minimum of 0.5 ETH
 Sell STORJ for XBT. Keep a minimum balance of 50 USD worth of STORJ
 
 ```
-{
-  "name": "XXMR",
+"XXMR": {
   "minBalance": 10,
   "quote": "ZUSD",
   "buy": "XXBT"
 },
 ```
-Older pairs use the X, Z format for crypto and fiat. You will need to specity those.
-This setting will sell XMR for XBT while keeping a minimul balance of 10 USD worth of XMR
+Older pairs use the X/Z format for crypto/fiat. You will need to specity those explicitly. [Find your ticker](https://support.kraken.com/hc/en-us/articles/360000920306-Ticker-pairs).
 
-### fees
--- stack.py
+++ stack.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import krakenex
-import json 
+import json
 
 k = krakenex.API()
 k.load_key('kraken.key')
@@ -19,23 +19,21 @@ def getBalance():
 def getTradableBalance(bal):
     tb = {}
     for alt in c['alts']:
-        tb[alt['name']] = float(bal[alt['name']])      
+        tb[alt] = float(bal[alt])
     return tb
 
-    
+
 def getPrice(asset, units):
     pair = asset+units
-    print(pair)
     ticker = k.query_public('Ticker', {'pair': pair})
-    print(ticker)
     last_close_raw = ticker["result"][pair]["c"]
     last_close = last_close_raw[0]
     return float(last_close)
 
 def getAltConfig(ticker):
     for alt in c['alts']:
-        if alt['name'] == ticker:
-            return alt
+        if alt == ticker:
+            return c['alts'][alt]
 
 def getSellableUnits(tradable):
     # units of each tradable asset can be sold
@@ -45,15 +43,41 @@ def getSellableUnits(tradable):
         unitsOfMin = a['minBalance'] / getPrice(alt, a['quote'])
         if tradable[alt] > unitsOfMin:
             sellable[alt] = tradable[alt] - unitsOfMin
-        else:
-            sellable[alt] = 0
     return sellable
 
-def main():
+def getPair(alt):
+  return "%s%s" % (alt, c['alts'][alt]['buy'])
+
+def marketBuy(pair, amt):
+    print("BUY %s %s" % (pair, amt))
+
+def worthIt(alt, amt):
+    return True
+
+def processTrades():
     bal = getBalance()
+    printTable(bal, "Account Balances:")
     tradable = getTradableBalance(bal)
-    print(tradable)
-    print(getSellableUnits(tradable))
+    sellable = getSellableUnits(tradable)
+    if sellable:
+        printTable(sellable, "Ready to Trade:")
+    else:
+        print("No alts to trade. Good job!")
+    for alt in sellable:
+        amt = sellable[alt]
+        if worthIt(alt, amt):
+	        pair = getPair(alt)
+	        marketBuy(pair, amt)
+
+def printTable(vals, desc):
+    print()
+    print(desc)
+    for key in vals:
+        print("%s %s" % (key, vals[key]))
+    print()
+
+def main():
+    processTrades()
     print("Done.")
 
 if __name__ == "__main__":
