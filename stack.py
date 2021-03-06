a696ca0bbbb57c7f9724f1afa88ca34c523a7015
count-null <70529195+count-null@users.noreply.github.com>
2021-03-06 22:20:32 +0000

    it stacks sats

--- stack.py
+++ stack.py
@@ -3,14 +3,34 @@
 import krakenex
 import json
 
+projectPath = '/home/admin/download/kraken-auto-stack/'
+
 k = krakenex.API()
-k.load_key('kraken.key')
+k.load_key(projectPath + 'kraken.key')
 
 def getConfig():
-    with open('config.json') as f:
+    with open(projectPath + 'config.json') as f:
         return json.load(f)
 
 c = getConfig()
+
+def configLegit():
+    keys = ['minBalance', 'minSell', 'maxSell', 'quote', 'buy']
+    for alt in c['alts']:
+        # Check all the config keys are there
+        for key in keys:
+            if key not in c['alts'][alt]:
+                print("Configuration error!")
+                print("%s is missing key [%s]" % (alt, key))
+                return False
+        # Check minBuy is less than maxBuy (if maxBuy is set)
+        maxSell = c['alts'][alt]['maxSell']
+        minSell = c['alts'][alt]['minSell']
+        if maxSell and minSell > maxSell:
+            print("Configuration error!")
+            print("[maxBuy] must be less than [minBuy] for %s" % (alt))
+            return False
+    return True
 
 def getBalance():
     return k.query_private('Balance')['result']
@@ -20,6 +40,13 @@ def getTradableBalance(bal):
     for alt in c['alts']:
         tb[alt] = float(bal[alt])
     return tb
+
+def getMinOrder(alt):
+    a = getAltConfig(alt)
+    pair = alt+a['quote']
+    response = k.query_public('AssetPairs', {'pair': pair})
+    orderMin = response['result'][pair]['ordermin']
+    return orderMin
 
 def getPrice(asset, quote):
     pair = asset+quote
@@ -54,41 +81,50 @@ def getPair(alt):
 
 def marketBuy(alt, amt):
     pair = getPair(alt)
-    k.query_private('AddOrder', {
+    response = k.query_private('AddOrder', {
         'pair': pair,
-        'type': 'buy',
+        'type': 'sell',
         'ordertype': 'market',
-        'volume': amt
+        'volume': str(amt)
     })
-    print("BUY %s %s" % (pair, amt))
+    print("%s txid %s" % (response['result']['descr']['order'], response['result']['txid'][0]))
 
 def worthIt(alt, amt):
     a = getAltConfig(alt)
-    return getPrice(alt, amt) > a['minSell']
+    return getPrice(alt, a['quote'])*amt > a['minSell']
 
 def getVolume(alt, amt):
     a = getAltConfig(alt)
     if a['maxSell']:
         stackSize = a['maxSell'] / getPrice(alt, a['quote'])
         if amt > stackSize:
-            return stackSize
-    return amt
+            return round(stackSize, 4)
+    return round(amt, 4)
 
 def processTrades():
-    #bal = getBalance()
-    bal = {"STORJ": 1234.2342234, "XXMR": 3423.232355}
+    bal = getBalance()
     printTable(bal, "Account Balances:")
     tradable = getTradableBalance(bal)
     reserves = getReserveUnits(tradable)
     printTable(reserves, "Minimum Required Balances:")
     sellable = getSellableUnits(tradable, reserves)
     if sellable:
-        printTable(sellable, "Ready to Trade:")
+        aboutToSell={}
+        minOrders={}
         for alt in sellable:
             amt = sellable[alt]
             if worthIt(alt, amt):
                 vol = getVolume(alt, amt)
-    	        marketBuy(alt, vol)
+                minOrder = getMinOrder(alt)
+                aboutToSell[alt] = vol
+                minOrders[alt] = minOrder
+        printTable(aboutToSell, "Eligible to Trade:")
+        printTable(minOrders, "Kraken Min Orders:")
+        for alt in aboutToSell:
+            if aboutToSell[alt] > float(minOrders[alt]):
+                marketBuy(alt, aboutToSell[alt])
+            else:
+                print("%s volume does not meet Kraken's minimum requirements for trade." % (alt))
     else:
         print("No alts to trade. Good job!")
 
@@ -101,7 +137,8 @@ def printTable(vals, desc):
     print()
 
 def main():
-    processTrades()
+    if configLegit():
+        processTrades()
     print("Done.")
 
 if __name__ == "__main__":
